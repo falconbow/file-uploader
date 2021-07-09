bfec04dfe611450896796a45bd167b1ebea8f6fd
Nikita Ruseev <ruseev.nikita@gmail.com>
2021-07-09 16:10:33 +0300

    remove toastr

--- src/main.js
+++ src/main.js
@@ -1,17 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import { store } from "../src/store/index";
-import VueToastr from "vue-toastr";
 import "./assets/scss/set.scss";
 import "./assets/scss/fonts.scss";
-Vue.use(VueToastr, {});
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  mounted() {
-    this.$toastr.defaultPosition = "toast-top-right";
-  },
+  mounted() {},
 
   render: (h) => h(App),
 }).$mount("#app");
